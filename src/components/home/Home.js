d314e75ea7d10190018af1aac39f746bae80a69c
omar elk <o.elk09@gmail.com>
2022-04-25 02:28:24 -0400

    Co-authored-by: gianfrancodumoulin@hotmail.com <gianfrancodumoulin@hotmail.com>

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -5,8 +5,21 @@ import './home.scss'
 export default function Home() {
 	return (
 		<div className = 'home-container'>
-			<p>hello</p>
-			<img src="/images/logo.png" alt="logo"/>
+			<div className = 'image-logo'>
+				<img  src="/images/img2.jpeg" alt="logo"/>
+				<div className='search-bar-container'>
+					<input 
+						className = "search-bar"
+						type="text"
+						placeholder = "Search for an item to rent..."
+						required 
+
+					/>
+					{/* <button>
+
+					</button> */}
+				</div>
+			</div>	
 		</div>
 	)
 }
---
+++ src/components/rent/Rent.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './rent.scss'
+// import {Outlet, Link} from 'react-router-dom'
+
+export default function Rent() {
+	return (
+		<div className = 'rent-container'>
+			<div className = 'image-logo'>
+				<img  src="/images/logo9.jpeg" alt="logo"/>
+			</div>	
+		</div>
+	)
+}
