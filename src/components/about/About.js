96ba90f90b00690dc43e884f7d4dc6528075e729
Mohamad El Chanati <mohammed_elchanati@hotmail.com>
2022-04-25 03:25:43 -0400

    addedaboutus

---
+++ src/components/about/About.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import './about.scss'
+// import {Outlet, Link} from 'react-router-dom'
+
+export default function About() {
+	return (
+		<div >
+
+				
+				<div className = 'home-container'>
+               
+					<p>
+                        We a group of Bachelors students and enthusiasts in Concordia University who wants to conteibute to the community and improve people's lives. We are based in Montreal, Quebec.
+                        We were inspired to develop RentAll after seeing and talking to people who are facing difficult financial situation and people who wants to reduce waste. We developed this
+                        application as part of the SOEN 357 final project.  
+                    </p>
+				</div>
+                <div >
+					<p> 
+                        For any information or suggestion, please contact us 
+                    </p>
+                    <p> 
+                        Mohamad El Chanati
+                        mohammed_elchanati@hotmail.com
+                    </p>
+                    <p> 
+                        Mohamad El Chanati
+                        mohammed_elchanati@hotmail.com
+                    </p>
+                    <p> 
+                        Mohamad El Chanati
+                        mohammed_elchanati@hotmail.com
+                    </p>
+                    <p> 
+                        Mohamad El Chanati
+                        mohammed_elchanati@hotmail.com
+                    </p>
+                    <p> 
+                        Mohamad El Chanati
+                        mohammed_elchanati@hotmail.com
+                    </p>
+				</div>
+			
+		</div>
+	)
+}
