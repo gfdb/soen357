5b254be7fa34cfcc0bbd89237e501f70bca5125a
Mohamad El Chanati <mohammed_elchanati@hotmail.com>
2022-04-26 00:34:07 -0400

    g

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -39,7 +39,6 @@ export default function About() {
                 
             </section>
 				
-<<<<<<< HEAD
 				<div className='para'>
                
 					<p class='paragraph' >
@@ -52,44 +51,6 @@ export default function About() {
                     </p>
 				</div>
               
-=======
-				<div className = 'about-container'>
-               
-					<p className='about-text'>
-                        We a group of Bachelors students and enthusiasts in Concordia University who wants to conteibute to the community and improve people's lives. We are based in Montreal, Quebec.
-                        We were inspired to develop RentAll after seeing and talking to people who are facing difficult financial situation and people who wants to reduce waste. We developed this
-                        application as part of the SOEN 357 final project.  
-                    </p>
-				</div>
-                <div className = 'home-container' >
-					<p> 
-                        For any information or suggestion, please contact us 
-                    </p>
-                    <p> 
-                        Mohamad El Chanati
-                        mohammed_elchanati@hotmail.com
-                        <br></br>
-                        <br></br>
-                        Mohamad El Chanati
-                        mohammed_elchanati@hotmail.com
-                        <br></br>
-                        <br></br>
-                        Mohamad El Chanati
-                        mohammed_elchanati@hotmail.com
-                        <br></br>
-                        <br></br>
-                        Mohamad El Chanati
-                        mohammed_elchanati@hotmail.com
-                        <br></br>
-                        <br></br>
-                        Mohamad El Chanati
-                        mohammed_elchanati@hotmail.com
-                        <br></br>
-                        <br></br>
-                    </p>
-                    
-				</div>
->>>>>>> 272c9b14afec865219e0b008d046ee9ef6b1e168
 			
 		</div>
 	)
